943d9ecdb8129f8f57e49992f2af3df45fc9b745
Oleg_Valerievich <vitiukoleh@gmail.com>
2018-03-11 02:26:46 +0200

    header done!

--- src/containers/components/TopHeader/index.js
+++ src/containers/components/TopHeader/index.js
@@ -8,13 +8,31 @@ export default class TopHeader extends React.Component {
 
     render() {
         return (
-            <div>
-                <div className='info'></div>
-                <ul className='menu'>
-                    {
-                        config.menu.map((item,index) => <li key={index}><Link to={`/${item.route}`} className='menu__item'>{item.name}</Link></li>)
-                    }
-                </ul>
+            <div className='topheader'>
+                <div className='topheader__info'>
+                    <div className="topheader__info-call">
+                        <img className='topheader__info-logo' src="https://ucarecdn.com/56ec26cb-df1b-4f55-8acc-827510b0ed81/1jojl1FOMkX9WypfBe43D6kivaHrx9NnxbJwXs1M3EMoAJtlSItgfBr9Psy.png" alt="logo"/>
+                        <div className="topheader__info-call-item topheader__info-call-form">Обратный звонок</div>
+                        <span className="topheader__info-call-item">0956289359</span>
+                        <div className="topheader__info-call-item topheader__info-call-form">Бесплатно</div>
+                    </div>
+                    <div className='topheader__info-search'>
+                        <div className="topheader__info-call-item topheader__info-call-form">Киев</div>
+                        <div className="topheader__info-call-item topheader__info-call-form">Поиск</div>
+                    </div>
+                </div>
+                <div className='topheader__menu'>
+                    <ul className='topheader__menu-navigation'>
+                        {
+                            config.menu.map((item,index) => <li key={index}><Link to={`/${item.route}`} className='topheader__menu-item'>{item.name}</Link></li>)
+                        }
+                    </ul>
+                     <ul className="topheader__menu-posibilities">
+                         {
+                             config.posibilities.map((item,index) => <li key={index}><Link to={`/${item.route}`} className='topheader__menu-item'>{item.name}</Link></li>)
+                         }
+                     </ul>
+                </div>
             </div>
         );
     }
