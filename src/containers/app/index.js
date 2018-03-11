a2ae1821c9f584c59e316100d55183003f5ca6f1
Oleg_Valerievich <vitiukoleh@gmail.com>
2018-03-11 13:56:38 +0200

    intro done!

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
-import Home from '../home';
-import Doctors from '../doctors';
-import Clinics from '../clinics';
+import Home from '../routes/home';
+import Doctors from '../routes/doctors';
+import Clinics from '../routes/clinics';
 import TopHeader from '../components/TopHeader';
+import Footer from '../components/Footer';
 
 const App = () => (
     <div>
         <TopHeader />
-
-        {/*<Link to="/">Home</Link>*/}
-{/*<Link to="/doctors">Doctor</Link>*/}
-
         <main>
             <Route exact path="/" component={Home} />
             <Route exact path="/doctors" component={Doctors} />
             <Route exact path="/clinics" component={Clinics} />
         </main>
+        <Footer />
     </div>
 );
 
---
+++ src/containers/routes/home/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {getUsers} from '../../../actions/userActions';
+import { Link } from 'react-router-dom';
+import './index.scss';
+import config from './config';
+
+class Home extends React.Component {
+
+    render() {
+        return (
+            <div className='home'>
+                <div className="home__main">
+                    <div className="main__welcome">
+                        <h1 className='main__greet'>Большой выбор врачей и клиник</h1>
+                        <span className="main__subtitle">Сравните информацию по рейтингам, отзывам, цене и подберите специалиста для себя и ваших близких в удобном районе</span>
+                    </div>
+                    <input className="home__search" type="search"
+                           placeholder="Специальность врача, процедура, имя доктора..."/>
+                    <div className="home__popular">
+                        <p className="home__popular-text">Популярные запросы</p>
+                        <ul className="home__popular-list">
+                            {
+                                config.popularItems.map((item, index) => <li key={index}><Link to={`${item.route}`}
+                                                                                               className='home__popular-list-item'>{item.name}</Link>
+                                </li>)
+                            }
+                        </ul>
+                    </div>
+                </div>
+                <div className="home__appointment"></div>
+                <div className="home__advantages"></div>
+                <div className="home__best"></div>
+                <div className="home__lastReviews"></div>
+                <div className="home__blog"></div>
+            </div>
+        );
+    }
+}
+
+export default connect(state => ({
+    users: state.users
+}))(Home)
