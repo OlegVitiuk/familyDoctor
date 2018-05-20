83ffbdba37ea74526361685324d16a2827335c74
Oleg_Valerievich <vitiukoleh@gmail.com>
2018-05-20 16:43:41 +0300

    set api in production

--- src/actions/doctor.js
+++ src/actions/doctor.js
@@ -4,5 +4,5 @@ import {GET_ALL_DOCTORS} from "constants/index";
 export const getAllDoctors = () => dispatch =>{
     api.get('/doctors/getAll').then(response => {
         dispatch({type: GET_ALL_DOCTORS, doctors: response.data});
-    }).catch(err => console.log(err));
+    }).catch(err => console.log(err.message));
 }
--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 let apiUrl = "http://localhost:8080";
 
 console.log(process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'production') {
+if (process.env.ENV === 'production') {
     console.log("set new Api");
     apiUrl = "https://enigmatic-journey-33069.herokuapp.com";
 }
