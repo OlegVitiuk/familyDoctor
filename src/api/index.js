5ab62f3a9fd9ca7076053798d76202d8a49ccb80
Oleg_Valerievich <vitiukoleh@gmail.com>
2018-05-21 01:41:56 +0300

    done environments!

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 let apiUrl = 'localhost:8080';
 
-if(process.env.REACT_APP_ENV === 'production'){
+if(process.env.REACT_APP_ENV){
     apiUrl  = 'https://enigmatic-journey-33069.herokuapp.com';
 }
 
