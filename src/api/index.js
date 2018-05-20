83b3853a1d2a7fb98e571f99b437ca6712d4affe
Oleg_Valerievich <vitiukoleh@gmail.com>
2018-05-21 02:00:40 +0300

    fix bugs!

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 let apiUrl = 'localhost:8080';
 
 console.log(process.env.REACT_APP_ENV);
+console.log(process.env.ENV);
 if(process.env.REACT_APP_ENV){
     apiUrl  = 'https://enigmatic-journey-33069.herokuapp.com';
 }
