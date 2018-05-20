78ab626d0000483bb430bf8f7bc82707488e1c53
Oleg_Valerievich <vitiukoleh@gmail.com>
2018-05-21 01:16:35 +0300

    fix bugs!

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const localApi = 'localhost:8080';
+
 export const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: process.env.REACT_APP_API_URL || localApi,
     headers: {
         'Content-Type': 'application/json',
     },
