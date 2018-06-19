ed3c2a4446846f6a151e44b33d8378251b7321e5
Oleg_Valerievich <vitiukoleh@gmail.com>
2018-06-20 00:54:41 +0300

    reviews done!

--- src/containers/routes/dopInfo/index.js
+++ src/containers/routes/dopInfo/index.js
@@ -4,6 +4,27 @@ import PropTypes from 'prop-types';
 import {getAllDoctors} from "actions/doctor";
 import Rating from 'react-rating';
 
+const menuConfig = [
+    {
+        name: 'reviews',
+        value: 'Відгуки'
+    },
+    {
+        name: 'about',
+        value: 'Про лікаря'
+    },
+    {
+        name: 'treatment',
+        value: 'Лікування захворювань'
+    }
+];
+
+const ratingItemsConfig = {
+    qualification: "Кваліфікація лікаря: ",
+    price: "Ціна-якість: ",
+    attention: "Уважність лікаря: "
+};
+
 class DopInfo extends React.Component {
 
     static propTypes = {
@@ -15,17 +36,54 @@ class DopInfo extends React.Component {
     }
 
     state = {
-        doctorData: {}
+        doctorData: {},
+        activeMenuItem: 'reviews'
     };
 
-    componentWillReceiveProps(nextProps) {
+    changeMenuItem = (item) => this.setState(() => ({
+        activeMenuItem: item
+    }));
 
+    renderTab = (item) => {
+        if(this.state.activeMenuItem === 'reviews'){
+            return (
+                <div className='dop-info__content__tabs-reviews'>
+                    {
+                        item.reviews.map((review)=>(
+                            <div key={Symbol(review.value).toString()} className='dop-info__content__tabs-review'>
+                                <div className="dop-info__content__tabs-review__left">
+                                    <div className="dop-info__content__tabs-review__left-header">
+                                        <img src="https://ucarecdn.com/5fc424b2-e5f9-44cf-b270-eaca961fb3e8/w128h1281338911352handthumbsup.png"
+                                             alt="resultReview" className='dop-info__content__tabs-review__left-header__img'/>
+                                        <span className='dop-info__content__tabs-review__left-header__name'>Руслан</span>
+                                    </div>
+                                    <div className="dop-info__content__tabs-review__left-text">
+                                        {review.text}
+                                    </div>
+                                </div>
+                                <div className="dop-info__content__tabs-review__right">
+                                    <span className="dop-info__content__tabs-review__right-date">
+                                        {review.date}
+                                    </span>
+                                    {
+                                        Object.keys(review.rating).map(el => (
+                                            <div key={Symbol(el).toString()} className="dop-info__content__tabs-review__right-item">
+                                                <span className='dop-info__content__tabs-review__right-item-text'>{ratingItemsConfig[el]}</span>
+                                                <Rating readonly= {true} initialRating={review.rating[el]} emptySymbol='star-empty star-special' fullSymbol='star-full star-special'/>
+                                            </div>
+                                        ))
+                                    }
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            );
+        }
     }
-
     render() {
         const {doctor} = this.props;
         const doctorData = doctor.selectedItem || {};
-        console.log(doctorData)
 
         if (Object.keys(doctorData).length) {
             const {doctor} = doctorData;
@@ -46,15 +104,19 @@ class DopInfo extends React.Component {
                             {doctor.type.join(" ")}</span>
                                 <div className='dop-info__doctor-content-rating'>
                                     <span className='dop-info__doctor-content-rating-text'>Середня оцінка</span>
-                                    <Rating initialRating={doctor.rating} emptySymbol='star-empty star-special' fullSymbol='star-full star-special'/>
+                                    <Rating initialRating={doctor.rating} emptySymbol='star-empty star-special'
+                                            fullSymbol='star-full star-special'/>
                                     <span className='dop-info__doctor-content-rating-value'>{doctor.rating}</span>
                                     <div className="dop-info__doctor-content-reviews">
                                         {`${doctor.reviews.length} ${doctor.reviews.length === 1 ? 'відгук' : 'відгуків'}`}
                                     </div>
                                 </div>
                                 <div className="dop-info__doctor-content-experience">
-                                    <img className='dop-info__doctor-content-experience-img' src="https://ucarecdn.com/d142059c-c91c-445e-a490-09ecf6c94878/doc_exp.png" alt="experience logo"/>
-                                    <span className="dop-info__doctor-content-experience-text">{`Років досвіду: ${doctor.experience}`}</span>
+                                    <img className='dop-info__doctor-content-experience-img'
+                                         src="https://ucarecdn.com/d142059c-c91c-445e-a490-09ecf6c94878/doc_exp.png"
+                                         alt="experience logo"/>
+                                    <span
+                                        className="dop-info__doctor-content-experience-text">{`Років досвіду: ${doctor.experience}`}</span>
                                 </div>
                             </div>
                         </div>
@@ -62,7 +124,16 @@ class DopInfo extends React.Component {
 
                         </div>
                         <div className="dop-info__content">
-
+                            <div className="dop-info__content__tabs">
+                                {
+                                    menuConfig.map((item) => (
+                                        <span key={item.name} className={this.state.activeMenuItem === item.name ? 'active' : ''}
+                                              onClick={() => this.changeMenuItem(item.name)}>{item.value}</span>))
+                                }
+                            </div>
+                            {
+                                this.renderTab(doctor)
+                            }
                         </div>
                     </div>
                 </div>
